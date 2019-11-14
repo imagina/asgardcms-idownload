9848dcbae24413985c48e8f9a0f41f7823ad1c79
Yeison Tapia <yftapial@ut.edu.co>
2019-11-14 16:58:12 -0500

    + Add base Module

---
+++ Config/permissions.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'idownload.categories' => [
+        'index' => 'idownload::categories.list resource',
+        'create' => 'idownload::categories.create resource',
+        'edit' => 'idownload::categories.edit resource',
+        'destroy' => 'idownload::categories.destroy resource',
+    ],
+    'idownload.downloads' => [
+        'index' => 'idownload::downloads.list resource',
+        'create' => 'idownload::downloads.create resource',
+        'edit' => 'idownload::downloads.edit resource',
+        'destroy' => 'idownload::downloads.destroy resource',
+    ],
+    'idownload.suscriptors' => [
+        'index' => 'idownload::suscriptors.list resource',
+        'create' => 'idownload::suscriptors.create resource',
+        'edit' => 'idownload::suscriptors.edit resource',
+        'destroy' => 'idownload::suscriptors.destroy resource',
+    ],
+// append
+
+
+
+];
