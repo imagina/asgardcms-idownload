90344f2cce3842dbd2328ff4e86616a36edb4287
Yeison Tapia <yftapial@ut.edu.co>
2019-11-16 10:27:19 -0500

    + Add SendMail template view

--- Events/Handlers/HandlerSucriptorWasCreated.php
+++ Events/Handlers/HandlerSucriptorWasCreated.php
@@ -5,6 +5,8 @@ namespace Modules\Idownload\Events\Handlers;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Contracts\Mail\Mailer;
+use Modules\Idownload\Emails\Sendmail;
+use Modules\Media\Entities\File;
 
 class HandlerSucriptorWasCreated
 {
@@ -31,19 +33,34 @@ class HandlerSucriptorWasCreated
      */
     public function handle($event)
     {
-        /*Slim descomentar para probar*/
-      $subscription = $event->entity;
-      $download = $event->data['download'];
-      $sender = $this->setting->get('core::site-name');
-      $subject = $download->title . " " . trans('iforms::forms.messages.from') . " " . $sender;
-      $view = ['iforms::frontend.emails.form','iforms::frontend.emails.textform'];
+      /*Slim descomentar para probar*/
+      try {
+        $suscriptor = $event->suscriptor->toArray();
+        $download = $event->data['download'];
+        \Log::info('Sending email - '.$suscriptor['email']);
+        $suscriptor['download'] = "<a href='{$download->download_file->path}' target='_blank'>" . trans('idownload::downloads.button.download_link') . "</a>";
+        $sender = $this->setting->get('core::site-name');
+        $subject = $download->title . " " . $sender;
+        $view = ['idownload::frontend.emails.form', 'idownload::frontend.emails.textform'];
+        unset($suscriptor['id'],$suscriptor['download_id'],$suscriptor['created_at'],$suscriptor['updated_at']);
+        $dumpEmails = json_decode($this->setting->get('isite::emails'));
+        $emails2 = [];
+        foreach ($dumpEmails as $dumpEmail){
+          $emails2[] = $dumpEmail->value;
+        }
+        $emails2 =  count($emails2)>0?$emails2:[env('MAIL_FROM_ADDRESS')];
+        $emails[]= $suscriptor['email'];
 
-      $emails = !empty($this->setting->get('isite::emails'))?json_decode($this->setting->get('isite::emails'),true):[env('MAIL_FROM_ADDRESS')];
+        $view2 = ['idownload::frontend.emails.admin', 'idownload::frontend.emails.textadmin'];
 
-      if (isset($subscription->email) && !empty($subscription->email)) {
-        array_push($emails, $subscription->email);
+        $this->mail->to($emails)->send(new Sendmail(['lead' => $suscriptor, 'form' => $download], $subject, $view));
+        unset($suscriptor['download']);
+        $subject2 =  trans('idownload::idownloads.notification.admin'). ': '. $download->title . " " . $sender;
+        $this->mail->to($emails2)->send(new Sendmail(['lead' => $suscriptor, 'form' => $download], $subject2, $view2));
+
+        \Log::info('Successful email - '.$suscriptor['email']);
+      }catch(\Exception $e){
+        \Log::error($e->getMessage().' '.$e->getFile().' '.$e->getLine());
       }
-
-      $this->mail->to($emails)->send(new Sendmail(['lead'=>$subscription,'form'=>$download], $subject, $view));
     }
 }
--- Repositories/SuscriptorRepository.php
+++ Repositories/SuscriptorRepository.php
@@ -6,4 +6,7 @@ use Modules\Core\Repositories\BaseRepository;
 
 interface SuscriptorRepository extends BaseRepository
 {
+  public function create($data);
+  public function update($download, $data);
+  public function destroy($model);
 }
